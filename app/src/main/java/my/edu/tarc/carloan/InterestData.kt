20fbb906ce8e3c9c7a8a3278521512230ef35324
anreh <anurehka@gmail.com>
2022-08-05 09:52:05 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/carloan/InterestData.kt
@@ -0,0 +1,3 @@
+package my.edu.tarc.carloan
+//TODO2 : DEFINE A DATA CLASS
+class InterestData (var bank: String ="", var interest: String ="") {}
---
+++ app/src/main/java/my/edu/tarc/carloan/SecondFragment.kt
@@ -0,0 +1,60 @@
+package my.edu.tarc.carloan
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
+import my.edu.tarc.carloan.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+
+        //TODO 4: CREATE AN INSTANCE OF DATA BINDING
+
+       /* _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root*/
+        _binding = DataBindingUtil.inflate(inflater,R.layout.fragment_second,container, false)
+        return binding.root
+    }
+    override fun onPrepareOptionsMenu(menu: Menu) {
+        super.onPrepareOptionsMenu(menu)
+        menu.findItem(R.id.action_about).isVisible = false
+        menu.findItem(R.id.action_settings).isVisible=false
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //TODO 5:Create an instance of data class
+        val interestData= InterestData( "CIMB\nMaybank\nPublic Bank\n",  "2.95%(p.a)\n3.01%(p.a)\n3.05\n3.15%(p.a)")
+//TODO 6:Link data class to layout
+        binding.bankInterestRate=interestData
+
+        //binding.textView.text = interestData.bank
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
